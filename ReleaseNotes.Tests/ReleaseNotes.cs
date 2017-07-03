c707e832fe89204b7b471c462669d6c481e41c80
INFOTRACK\michael.lachlan <michael.lachlan@infotrack.com.au>
2017-07-03 12:15:41 +1000

    Testing RestClient

---
+++ ReleaseNotes.Tests/ReleaseNotes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReleaseNotesBusinessLogic;
+
+namespace ReleaseNotes.Tests
+{
+    [TestClass]
+    public class ReleaseNotes
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var temp = new Service();
+
+            temp.SetJiraConnection();
+
+        }
+    }
+}
---
+++ ReleaseNotesBusinessLogic/Service.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Atlassian;
+using Atlassian.Jira;
+using Newtonsoft.Json;
+
+namespace ReleaseNotesBusinessLogic
+{
+    public class Service
+    {
+
+        string sampleRequestJson = @"{
+    'name': 'All Open Bugs',
+    'description': 'Lists all open bugs',
+    'jql': 'type = Bug and resolution is empty',
+    'favourite': true,
+    'favouritedCount': 0}";
+        private Jira _jiraConnection;
+
+
+        public Service()
+        {
+            SetJiraConnection();
+        }
+
+        public void SetJiraConnection()
+        {
+            var jira = Jira.CreateRestClient("https://infotrack.atlassian.net", "michael.lachlan", "Password1");
+
+            var issues = from i in jira.Issues.Queryable
+                         orderby i.Created
+                         select i;
+
+
+           
+
+        }
+
+
+
+
+        public string RunQueryRequest()
+        {
+
+
+            return null;
+
+
+        }
+
+    }
+}
